417c3c63576a2a2f969c1a11d03dbca0bc94028f
imu <imu@example.com>
2026-03-13 05:37:42 -0500

    fix up some more

--- includes/roblox/Humanoid.h
+++ includes/roblox/Humanoid.h
@@ -5,9 +5,9 @@ namespace RBX {
 	public: 
 	    Humanoid() = default;
 	    explicit Humanoid(uintptr_t instance_address) : Instance(instance_address){};
-	    void set_walkspeed(int walkspeed) const;
+	    void set_walkspeed(float walkspeed) const;
+		void set_walkspeed(int walkspeed) const;
 	private:
->>>>>>> 895b24ef88816d4bc9043b3873c0441dbd2d2e8e
 	    bool class_valid() const;
     };
 }

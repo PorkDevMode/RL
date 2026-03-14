a690411d43b04fc6087e85fb9ef0000a34ab3d9c
imu <imu@example.com>
2026-03-14 06:30:24 -0500

    Add template definitions to header

--- includes/roblox/FastFlag.h
+++ includes/roblox/FastFlag.h
@@ -1,9 +1,17 @@
 #pragma once
 #include <cstdint>
+#include "includes/memory/memory.h"
 
 namespace RBX {
     template<typename T>
-    T GetFastFlagValue(uintptr_t fflag_offset);
+    T GetFastFlagValue(uintptr_t fflag_offset) {
+        if (!mem.initialized) return {};
+        return mem.read<T>(mem.process_info.base + fflag_offset);
+    }
+
     template<typename T>
-    void SetFastFlagValue(uintptr_t fflag_offset, T value);
+    void SetFastFlagValue(uintptr_t fflag_offset, T value) {
+        if (!mem.initialized) return;
+        mem.write<T>(mem.process_info.base + fflag_offset, value);
+    }
 }

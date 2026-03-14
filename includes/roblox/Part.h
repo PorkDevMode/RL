da65820f37f7e171fb972dfa8478e3490cc58f60
imu <imu@example.com>
2026-03-14 01:22:41 -0500

    Add part class, 2ez

---
+++ includes/roblox/Part.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Instance.h"
+#include "BasePart.h"
+
+namespace RBX {
+    class Part : public BasePart {
+    public:
+        Part() = default;
+		explicit Part(uintptr_t instance_address) : BasePart(instance_address) {}
+    private:
+        bool valid_class() const;
+    };
+}

0a910ee3b95888405f7d1be67f049e90cf431b2e
imu <imu@example.com>
2026-03-13 22:31:39 -0500

    Add more definitions for humanoid class functions

--- includes/roblox/Humanoid.h
+++ includes/roblox/Humanoid.h
@@ -1,12 +1,60 @@
 #include "Instance.h"
+#include "Structs.h"
 
 namespace RBX {
     class Humanoid : public Instance {
 	public: 
 	    Humanoid() = default;
-	    explicit Humanoid(uintptr_t instance_address) : Instance(instance_address){};
+	    explicit Humanoid(uintptr_t instance_address) : Instance(instance_address){}
+
+		bool get_autojump() const;
+		void set_autojump(bool value) const;
+
+		bool get_use_jump_power() const;
+		void set_use_jump_power(bool value) const;
+
+		bool get_evaluate_state_machine() const;
+		void set_evaulate_state_machine(bool value) const;
+
+		bool get_sit() const;
+		void set_sit(bool value) const;
+
+		bool get_platform_stand() const;
+		void set_platform_stand() const;
+
+		std::string get_display_name() const;
+		void set_display_name() const;
+
+		HumanoidStateType get_humanoid_state() const;
+		void set_humanoid_state(HumanoidStateType type) const;
+
+		NameOcclusion get_name_occlusion() const;
+		void set_name_occlusion(NameOcclusion occlusion) const;
+
+		float get_walkspeed() const;
 	    void set_walkspeed(float walkspeed) const;
 		void set_walkspeed(int walkspeed) const;
+		
+		float get_hipheight() const;
+		void set_hipheight(float value) const;
+
+		float get_health() const;
+		void set_health(float value) const;
+
+		float get_max_health() const;
+		void set_max_health(float value) const;
+
+		float get_max_slope_angle() const;
+		void set_max_slope_angle(float value) const;
+
+		float get_jump_height() const; // make sure to check jump power enabled first
+		void set_jump_height() const;
+
+		float get_jump_power() const; // make sure jump power is enabled
+		void set_jump_power() const;
+
+		float get_health_display_distance() const;
+		void set_health_display_distance() const;
 	private:
 	    bool class_valid() const;
     };
--- includes/roblox/Structs.h
+++ includes/roblox/Structs.h
@@ -10,3 +10,36 @@ struct Vector3 {
 struct Vector2 {
 	float x, y;
 };
+
+enum HumanoidStateType {
+	FallingDown = 0,
+	Ragdoll = 1,
+	GettingUp = 2,
+	Jumping = 3,
+	Swimming = 4,
+	Freefall = 5,
+	Flying = 6,
+	Landed = 7,
+	Running = 8,
+	RunningNoPhysics = 10,
+	StrafingNoPhysics = 11,
+	Climbing = 12,
+	Seated = 13,
+	PlatformStanding = 14,
+	Dead = 15,
+	Physics = 16,
+	None = 18
+};
+
+enum NameOcclusion {
+	NoOcclusion = 0,
+	EnemyOcclusion = 1,
+	OccludeAll = 2
+};
+
+enum HumanoidRigType {
+	R15 = 0,
+	CustomHumanoid = 1,
+	Custom = 2,
+	NoneL = 3 // named NoneL if u dont like it fuck yourself
+};
